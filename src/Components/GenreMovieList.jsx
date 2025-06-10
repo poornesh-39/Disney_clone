52febe73b8aa14e4f3e4b96a2c08b87e009249da
Poornesh <poornesh@example.com>
2025-06-10 21:30:15 +0530

    updated commit

--- src/Components/GenreMovieList.jsx
+++ src/Components/GenreMovieList.jsx
@@ -6,7 +6,7 @@ function GenreMovieList() {
   return (
     <div>
         {GenresList.genere.map((item,index) =>index<5&&(
-            <div className='p-3 px-5 md:px-10'>
+            <div className='p-5 px-5 md:px-16'>
                 <h2 className='text-[20px] text-white font-bold'>{item.name}</h2>
                 <MovieList genreId={item.id} index_={index}/>
             </div>
